4b117dbd3f1b59f670eec7fcc30ca4a44e546cbf
Sören Titze <soeren.titze@gmx.de>
2016-05-21 16:02:15 +0200

    InputController

--- Assets/Scripts/Controller/GunController.cs
+++ Assets/Scripts/Controller/GunController.cs
@@ -3,11 +3,18 @@ using System.Collections;
 
 public class GunController : MonoBehaviour {
 
+    public Texture2D CrosshairTexture;
+
 	// Use this for initialization
 	void Start () {
-        
+        Cursor.SetCursor(CrosshairTexture, Vector2.zero, CursorMode.Auto);        
 	}
 	
+    void OnGUI()
+    {
+        
+    }
+
 	// Update is called once per frame
 	void Update () {
 	    
---
+++ Assets/Scripts/Controller/InputController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class InputController : MonoBehaviour {
+
+    public Action<Vector3> MouseMove;
+    public Action Fire1;
+
+    private Vector3 _lastPos;
+
+	// Update is called once per frame
+	void Update () {
+        var curPos = Input.mousePosition;
+        if (_lastPos != curPos)
+        {
+            _lastPos = curPos;
+            if (MouseMove != null) MouseMove(_lastPos);
+        }
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (Fire1 != null) Fire1();
+        }
+	}
+}
