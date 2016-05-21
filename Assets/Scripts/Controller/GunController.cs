b97dc5f04ade6536c9b7550c28ef4b73f9218519
Sören Titze <soeren.titze@gmx.de>
2016-05-21 17:39:52 +0200

    GunController Mouse Rotation update

--- Assets/Scripts/Controller/GunController.cs
+++ Assets/Scripts/Controller/GunController.cs
@@ -14,12 +14,25 @@ public class GunController : MonoBehaviour
 
     public void RotateTo(Vector3 to)
     {
-        var object_pos = Camera.main.WorldToScreenPoint(transform.position);
+        var cam = Camera.main;
+        var my = transform.position;
+        // Distance from camera to object.  We need this to get the proper calculation.
+        float camDis = cam.transform.position.y - transform.position.y;
+
+        // Get the mouse position in world spaaaaaace. Using camDis for the Z axis.
+        Vector3 mouse = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, camDis));
+
+        float AngleRad = Mathf.Atan2(mouse.y - transform.position.y, mouse.x - transform.position.x);
+        float angle = (180 / Mathf.PI) * AngleRad;
+
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+
+      /*  var object_pos = Camera.main.WorldToScreenPoint(transform.position);
         to.x = to.x - object_pos.x;
         to.y = to.y - object_pos.y;
         var angle = Mathf.Atan2(to.y, to.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle - 90));
-
+        */
     }
 
     public void Fire()
