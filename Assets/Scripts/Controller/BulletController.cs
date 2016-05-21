2203ac432faa07dafcc6141354a5a6d34204299c
Sören Titze <soeren.titze@gmx.de>
2016-05-21 16:19:36 +0200

    Works on My Machine?

---
+++ Assets/Scripts/Controller/BulletController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletController : MonoBehaviour {
+
+    private Vector3 _direction;
+    private float _speed;
+
+    public BulletController(Vector2 direction, float speed = 0)
+    {
+        _direction = direction;
+        _speed = speed;
+    }	
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += _direction * _speed;
+	}
+}
