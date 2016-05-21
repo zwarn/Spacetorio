b6e54b8ad15ada0d9b28aef0735ba32b4b7d81c2
zwarn <Michael.Budahn@gmail.com>
2016-05-21 17:23:16 +0200

    transport belt

---
+++ Assets/Scripts/Controller/GameController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour
+{
+
+    public static GameController Instance;
+    public State State;
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        State = new State(10);
+        for (int x = 0; x < 10; x++)
+        {
+            for (int y = 0; y < 10; y++)
+            {
+                State.getTile(x, y).Speed = 1;
+                State.getTile(x, y).Dir = ((x + y) % 2 == 0) ? Direction.North : Direction.East;
+            }
+        }
+    }
+
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
